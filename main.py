ef8b957fe940d108eb8ea6fdaabb763cfd346100
Rodennis22 <rodennis22@gmail.com>
2019-03-05 21:33:43 -0700

    adding main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Favorite Song Attributes 
+
+#Band information of favorite song
+Artist = "Collective Soul"
+Genre = "Alternative Rock/Pop"
+Location = "Georgia"
+RecordLabel = "Atlantic"
+
+#Song attributes
+LengthInMinutes = 5.07
+Album = "Hints Allegations and Things Left Unsaid"
+Key = "DMajor"
+ReleaseYear = 1994
+TrackNumber = 1
+WrittenBy = "Ed Roland"
