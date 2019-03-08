ca6b399c97fc45370c19ba9ea4efcdfa3cdc412d
Rodennis22 <rodennis22@gmail.com>
2019-03-07 17:42:39 -0700

    adding functions homework file

---
+++ functionshomework.py
@@ -0,0 +1,14 @@
+def Album():
+    album = "Hints Allegations and Things Left Unsaid"
+    return album
+
+def ReleaseYear():
+    year = 1994
+    return year
+
+def TrackNumber():
+    track = 1
+    return track
+
+def SoundOfTrack():
+    return True
