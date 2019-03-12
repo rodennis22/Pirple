c544299bc38b285b12aa908e2fbe222eaffff03f
Rodennis22 <rodennis22@gmail.com>
2019-03-12 09:23:08 -0600

    Added comments to code for Python HW5 Basic Loops

--- PythonHW5BasicLoops.py
+++ PythonHW5BasicLoops.py
@@ -5,6 +5,7 @@
 #multiples of 3 and 5 print FizzBuzz
 #prime numbers print Prime
 
+#function to check if number is prime
 def IsPrime(a):
     for count in range(2,a):
             if i%count == 0:
@@ -14,22 +15,25 @@ def IsPrime(a):
                 
 
 i = 1
-
+#iterate through 100
 while i <= 100:
-
+    #check if prime, print Prime if it is
     if IsPrime(i) == True:
         print("Prime")
         i +=1
-            
+    # check if divisible by 3 and 5, print FizzBuzz if is        
     elif i%3 == 0 and i%5 == 0:
         print("FizzBuzz")
         i += 1
+    #check if divisible by 3, print Fizz if it is    
     elif i%3 == 0:
         print("Fizz")
         i += 1
+    #check if divisible by 5, print Buzz if it is    
     elif i%5 == 0:
         print("Buzz")
         i += 1
+    #print number if not divisible by 3 or 5 and is not prime    
     else:
         print(i)
         i += 1
